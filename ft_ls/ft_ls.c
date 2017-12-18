c46443aaea0901ea3798e564a63974c647378de4
Carnagiul <pierrequeruel@gmail.com>
2017-12-18 20:54:59 +0100

    zae
    aze aze az

--- ft_ls/ft_ls.c
+++ ft_ls/ft_ls.c
@@ -113,7 +113,7 @@ int		ls_is_last(t_ls *ls, int nb, t_file *file, int id)
 {
 	if (id < nb && ls->cmd[0] == 1)
 		return (0);
-	while (id < nb)
+	while (id + 1 < nb)
 	{
 		if (file[id].name[0] == '.')
 			id++;
