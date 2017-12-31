3d136e43bb597048bf00368d36dd92a39f30bc0a
Carnagiul <pierrequeruel@gmail.com>
2017-12-31 02:04:37 +0100

    aze
    zaeazeae

--- ft_ls/ft_ls.c
+++ ft_ls/ft_ls.c
@@ -295,7 +295,7 @@ void				ft_display_ls_file(t_ls *ls, t_file_opt content)
 		ft_printf("%c", ft_display_file_type(content.stat));
 		ft_printf("%s ", content.mod);
 		ft_printf("%5ld ", content.stat.st_nlink);
-		ft_printf("%-*s ", ls->len_user, (content.pswd != NULL) ? content.pswd->pw_name : ft_itoa(content.stat.st_uid));
+		ft_printf("%-*s\t", ls->len_user, (content.pswd != NULL) ? content.pswd->pw_name : ft_itoa(content.stat.st_uid));
 		ft_printf("%-*s ", ls->len_group, (content.grp != NULL) ? content.grp->gr_name : ft_itoa(content.stat.st_gid));
 		ft_printf("%*lld ", ls->len_byte + 1,  content.stat.st_size);
 		//if (content.pswd)
