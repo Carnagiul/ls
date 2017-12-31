93ce19f39f8c573311294e5b87be5daf7586e569
Carnagiul <pierrequeruel@gmail.com>
2017-12-31 22:21:41 +0100

    azea
    ezaeazezaze

--- ft_ls/ft_ls.c
+++ ft_ls/ft_ls.c
@@ -289,13 +289,13 @@ char				ft_display_file_type(struct stat stat)
 	return ('-');
 }
 
-void				ft_display_timefile(char *timee, time_t timestamp)
+void				ft_display_timefile(time_t timestamp)
 {
 	char							**date;
 	char							**date_date;
 	time_t							t;
 
-	date = ft_strsplit(timee, ' ');
+	date = ft_strsplit(ctime(&timestamp), ' ');
 	date_date = ft_strsplit(date[3], ':');
 	ft_strdel_array(date_date);
 	ft_strdel_array(date);
@@ -316,7 +316,7 @@ void				ft_display_ls_file(t_ls *ls, t_file_opt content)
 		ft_printf("%-*s  ", ls->len_user, (content.pswd != NULL) ? content.pswd->pw_name : ft_itoa(content.stat.st_uid));
 		ft_printf("%-*s ", ls->len_group, (content.grp != NULL) ? content.grp->gr_name : ft_itoa(content.stat.st_gid));
 		ft_printf("%*lld ", ls->len_byte + 1,  content.stat.st_size);
-	    ft_display_timefile(ctime(&content.stat.st_ctime), content.stat.st_ctime);
+	    ft_display_timefile(content.stat.st_ctime);
 
 		//if (content.pswd)
 		//	free(content.pswd);
