a1f369e9ec1bb1948324054a6a7457097bf9b397
Carnagiul <pierrequeruel@gmail.com>
2018-01-01 19:01:20 +0100

    azeazeaze
    aeazeazeae

--- ft_ls/ft_ls.c
+++ ft_ls/ft_ls.c
@@ -201,6 +201,14 @@ t_file_ls			ft_get_files(char *path, t_ls *ls)
 	return (content);
 }
 
+void				x(int len, char *name)
+{
+
+	ft_putstr(name);
+	while (--len <= ft_strlen(name))
+		ft_putchar(' ');
+}
+
 void				ft_display_ls_file(t_ls *ls, t_file_opt content)
 {
 	int				test;
@@ -222,10 +230,12 @@ void				ft_display_ls_file(t_ls *ls, t_file_opt content)
 			ft_printf("%s ", "XXXXXXXXX");
 
 		ft_printf("%5ld ", content.stat.st_nlink);
-		if (content.pswd == NULL)
-			ft_printf("% - *.*s  ", ls->len_user + 1, ls->len_user + 1, ft_itoa(content.stat.st_uid));
-		else
-			ft_printf("% - *.*s  ", ls->len_user + 1, ls->len_user + 1, content.pswd->pw_name);
+		x(ls->len_user + 1, content.pswd->pw_name);
+		x(ls->len_group + 1, content.grp->gr_name);
+		//if (content.pswd == NULL)
+		//	ft_printf("% -*s  ", ls->len_user + 1, ls->len_user + 1, ft_itoa(content.stat.st_uid));
+		//else
+		//	ft_printf("% -*s  ", ls->len_user + 1, ls->len_user + 1, content.pswd->pw_name);
 		//if (content.grp == NULL)
 		//	ft_printf("%-*s  ", ls->len_group + 1, ft_itoa(content.stat.st_gid));
 		//else
